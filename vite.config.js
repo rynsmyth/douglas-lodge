6ed525851e09676c68813137d3298096ad3a305e
Ryan Smyth <rynsmyth@gmail.com>
2024-03-31 17:35:46 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': resolve(__dirname, 'src/components'),
-      // other aliases
-    },
-  },
+  base: '/douglas-lodge/',
+  plugins: [react()]
 });
-
